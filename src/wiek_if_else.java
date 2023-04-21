725ad64ae388f4b7c2c34fd77878c71c1047073c
popo <jojo@popo.com>
2023-04-21 17:30:23 +0200

    OK

---
+++ src/wiek_if_else.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class wiek_if_else {
+    public static void main(String[] args) {
+        System.out.println("Podaj swoj wiek: 1");
+        Scanner userInput = new Scanner(System.in);
+            int userAge = userInput.nextInt();
+
+            if (userAge < 18) {
+            System.out.println("Przepraszam, mlodzierzy alkoholi nie sprzedajemy!");
+        } else {
+            System.out.println("Dziekujemy za zakupy.");
+        }
+    }
+}
