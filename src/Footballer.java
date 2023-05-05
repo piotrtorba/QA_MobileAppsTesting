b2f6cd6c98beed6bd10a9d3db9f07c979c742b17
popo <torba.piotr@gmail.com>
2023-05-05 22:28:32 +0200

    none

--- src/Footballer.java
+++ src/Footballer.java
@@ -1,4 +1,4 @@
-public class Footballer {
+public class Footballer extends Person {
     public String club;
     public void play() {
         System.out.println("I play football");
---
+++ src/MathTeacher.java
@@ -0,0 +1,6 @@
+public class MathTeacher extends Person {
+    public String school;
+    public void teach() {
+        System.out.println("Im teaching Math");
+    }
+}
