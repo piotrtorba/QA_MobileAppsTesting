ce5462f1fba201c9ff9d596ea25ee075843df168
root <torba.piotr@gmail.com>
2023-05-06 18:46:15 +0200

    Homework (Inharitage, construktors & 'super')

--- src/AppsRun.java
+++ src/AppsRun.java
@@ -2,9 +2,11 @@ public class AppsRun {
     public static void main(String[] args) {
 
         AndroidApp mailAndroidApp = new AndroidApp("Mail Android App.");
+        mailAndroidApp.appInfo();
         mailAndroidApp.runAndriodApp();
 
         iPhoneApp mialIphoneApp = new iPhoneApp("Mail iPhone App");
+        mialIphoneApp.appInfo();
         mialIphoneApp.runIphoneApp();
     }
 }
