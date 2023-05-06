aea57a50a32c167f03bced2bf59d3af398b7c62e
root <torba.piotr@gmail.com>
2023-05-06 18:00:02 +0200

    None

--- src/Footballer.java
+++ src/Footballer.java
@@ -1,6 +1,17 @@
 public class Footballer extends Person {
     public String club;
+
+    public Footballer(String name, int age, String club) {
+        super(name, age);
+        this.club = club;
+    }
+
     public void play() {
         System.out.println("I play football");
+    }
+    public void sayHello() {
+        System.out.println("Hi! My name is... My name is ..." + name);
+        System.out.println("I'm " + age + " yrs old");
+        System.out.println("I play in " + club);
     }
 }
--- src/MathTeacher.java
+++ src/MathTeacher.java
@@ -1,6 +1,17 @@
 public class MathTeacher extends Person {
     public String school;
+
+    public MathTeacher(String name, int age, String school) {
+        super(name, age);
+        this.school = school;
+    }
+
     public void teach() {
         System.out.println("Im teaching Math");
+    }
+    public void sayHello() {
+        System.out.println("Hi! My name is... My name is ..." + name);
+        System.out.println("I'm " + age + " yrs old");
+        System.out.println("I teach in " + school);
     }
 }
