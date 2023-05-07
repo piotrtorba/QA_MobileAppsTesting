3ec853dfbfd14889ca03cbff6b3aba40c8b9c470
root <torba.piotr@gmail.com>
2023-05-07 18:13:18 +0200

    Wyjatki (Checked)

---
+++ src/CheckedExceptions.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CheckedExceptions {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Loading file...");
+            readFile("/home/popo/Documents/code/QA_TestJava/QA_TestJava/src/test.txt");
+            System.out.println("Job done!");
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception skipped");
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("Closing file...");
+        }
+    }
+    public static void readFile(String path) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(path);
+    }
+}
