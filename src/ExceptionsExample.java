2f29210da44c08ee665dc0160b42368691d4af3b
root <torba.piotr@gmail.com>
2023-05-07 14:17:11 +0200

    Wyjatki (Unchecked)

---
+++ src/ExceptionsExample.java
@@ -0,0 +1,31 @@
+import HomeWork_WebDriver.ChromeDriver;
+import HomeWork_WebDriver.FirefoxDriver;
+import HomeWork_WebDriver.WebDriver;
+
+public class ExceptionsExample {
+    public static void main(String[] args) {
+        /*int[] numbers = new int[2];
+        numbers[0] = 1;
+        numbers[1] = 3;
+
+        for (int i=0; i <= numbers.length; i++) { //'i' nie moze byc <= poniewaz rzeczywisty rozmiar tablicy to 3
+                                                //,a tzreci element nie istnieje
+            System.out.println(numbers[i]);
+        }*/
+
+        WebDriver driver = getDriver("firefoxaa");
+        driver.get();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+    }
+
+        private static WebDriver getDriver(String name){ //Logika metody getDriver()
+            if (name.equals("chrome")) {
+                return new ChromeDriver();
+            } else if (name.equals("firefox")) {
+                return new FirefoxDriver();
+            }
+            return null;
+    }
+}
